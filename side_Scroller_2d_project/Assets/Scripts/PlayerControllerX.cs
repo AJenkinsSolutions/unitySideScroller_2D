a78f19c73821805dbd122d5a30490873a7c5a1b8
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2022-04-10 15:25:57 -0500

    Add Vfx, Add Sfx, Add Particle Effects, Add Triggered Sfx, Game Over Bug fixes

--- side_Scroller_2d_project/Assets/Scripts/PlayerControllerX.cs
+++ side_Scroller_2d_project/Assets/Scripts/PlayerControllerX.cs
@@ -7,45 +7,84 @@ public class PlayerControllerX : MonoBehaviour
     
     private Rigidbody playerRb;
     private Animator playerAnimator;
+    AudioSource playerAudio;
+    
+    // Reference: public Particle System effect
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    public ParticleSystem dirtSplatterVFX;
+    public ParticleSystem explosionParticleVFX;
+
 
     public bool spaceInput;
     public bool isOnGround = true;
     public float jumpHeight;
+
     public bool gameOver = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // GET-Assign:  playerRigidBody components
         playerRb = GetComponent<Rigidbody>();
         playerAnimator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        spaceInput = Input.GetKeyDown(KeyCode.Space);
-
-        if (spaceInput & isOnGround & !gameOver)
+        if(!gameOver)
         {
-            playerRb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
-            playerAnimator.SetTrigger("Jump_trig");
-            isOnGround = false;
+            spaceInput = Input.GetKeyDown(KeyCode.Space);
+
+            if (spaceInput & isOnGround & !gameOver)
+            {
+                //Clean Up
+                dirtSplatterVFX.Stop();
+                isOnGround = false;
+
+                // Mover 
+                playerRb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+
+                //Animation
+                playerAnimator.SetTrigger("Jump_trig");
+                // SFX
+                playerAudio.PlayOneShot(jumpSound, 1f);
+
+
+            }
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        
+        if(!gameOver)
         {
-            isOnGround = true;
-            Debug.Log("IsOnGround");
-        }
-        if (collision.gameObject.CompareTag("Obstacle"))
-        {
-            gameOver = true;
-            Debug.Log("GameOver");
-            playerAnimator.SetBool("Death_b", true);
-            playerAnimator.SetInteger("DeathType_int", 1);
+            if (collision.gameObject.CompareTag("Ground"))
+            {
+                isOnGround = true;
+                dirtSplatterVFX.Play();
+
+                Debug.Log("IsOnGround");
+            }
+            if (collision.gameObject.CompareTag("Obstacle"))
+            {
+                // Clean up
+                gameOver = true;
+                dirtSplatterVFX.Stop();
+                Debug.Log("GameOver");
+
+                //  CrashAnimation
+                playerAnimator.SetBool("Death_b", true);
+                playerAnimator.SetInteger("DeathType_int", 1);
+
+                //  Crash SFX-VFX
+                playerAudio.PlayOneShot(crashSound, 1f);
+                explosionParticleVFX.Play();
+
+
+            }
         }
     }
 }
